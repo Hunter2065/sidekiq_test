3a53e5096a1028e4397c12cc48d9b6fabd66a19d
Ivan <vanuha277@gmail.com>
2021-08-06 18:36:19 +0300

    added sidekiq configs

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,12 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SidekiqTest
+module BackgroundJobsWithSidekiq
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    config.active_job.queue_adapter = :sidekiq
 
     # Configuration for the application, engines, and railties goes here.
     #
