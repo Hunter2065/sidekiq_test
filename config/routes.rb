0b0969601f5cd859d5c26eb924eeda8968b14604
Ivan <vanuha277@gmail.com>
2021-08-06 18:35:40 +0300

    added routes for sidekiq UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+
   root 'home#index'
+
+  mount Sidekiq::Web => '/sidekiq'
 
   get 'destroy_all', to: 'home#destroy_all'
   get 'create_random_user', to: 'home#create_random_user'
