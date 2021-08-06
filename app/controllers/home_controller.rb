1ad5c7f223e2349a81cf93955adf3d2afdfd81b1
Ivan <vanuha277@gmail.com>
2021-08-06 12:05:46 +0300

    added mechanizm destroying and creating

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,27 @@
 class HomeController < ApplicationController
+  before_action :index, :get_users
+
   def index
   end
+
+  def destroy_all
+    User.all.destroy_all
+    redirect_to root_path
+  end
+
+  def create_random_user
+    user = User.new
+    user.first_name = Faker::Name.first_name
+    user.last_name = Faker::Name.last_name
+    user.email = Faker::Internet.email
+    user.save!
+    sleep 2
+    redirect_to root_path
+  end
+
+  private
+
+    def get_users
+      @users = User.all
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  get 'destroy_all', to: 'home#destroy_all'
+  get 'create_random_user', to: 'home#create_random_user'
 end
